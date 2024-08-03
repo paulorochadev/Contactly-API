dcd75cfc1da7630b6cce5e000ac0b506909fe1ad
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-03 12:56:40 -0300

    feat: Cria endpoint GetAllContacts

---
+++ Contactly-API/Controllers/ContactsController.cs
@@ -0,0 +1,27 @@
+﻿using Contactly_API.Models.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contactly_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactlyDbContext _dbContext;
+
+        public ContactsController(ContactlyDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = _dbContext.Contacts.ToList();
+
+            return Ok(contacts);
+        }
+
+    }
+}
