02d77b2a3214be1194802fead23e0dec30d284de
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-03 12:44:43 -0300

    chore: Adiciona conexão com BD

--- Contactly-API/Program.cs
+++ Contactly-API/Program.cs
@@ -1,3 +1,6 @@
+using Contactly_API.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContactlyDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
 
 var app = builder.Build();
 
