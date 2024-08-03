ca23470e2c05224e7b361c564c14b854eef89a32
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-03 12:28:06 -0300

    chore: Adiciona EF Core InMemory e Model Data

---
+++ Contactly-API/Models/Data/ContactlyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Contactly_API.Models.Data
+{
+    public class ContactlyDbContext : DbContext
+    {
+        public ContactlyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
