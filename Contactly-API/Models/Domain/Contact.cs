62683a3213944a7492a23922cb6ea36a36e867b4
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-03 12:30:49 -0300

    chore: Adiciona classe Contact na Models

---
+++ Contactly-API/Models/Domain/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace Contactly_API.Models.Domain
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
